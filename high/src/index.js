05899cb654898c465818ca71fffe2d2a941e1c64
vistritpandey <vistritpandey@gmail.com>
2020-12-10 09:28:33 +0530

    Stonks!!!!!!!!!!1

--- high/src/index.js
+++ high/src/index.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import { render } from 'react-dom'
-import HighCharts from 'highcharts'
-import HighChartsReact from 'highcharts-react-official'
+import React from 'react';
+import { render } from 'react-dom';
+import Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
 
 const options = {
-    chart : {
-        type: 'spline'
-    },
-    title : {
-        text: 'My Chart'
-    },
-    series : [
-        {
-            data: [1,2,1,4,3,6]
-        }
-    ]
-}
+  title: {
+    text: 'My stock chart'
+  },
+  series: [
+    {
+      data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9]
+    }
+  ]
+};
 
 const App = () => (
-    <div>
-        <HighChartsReact highCharts={HighCharts} options={options} />
-    </div>
+  <div>
+    <HighchartsReact
+      highcharts={Highcharts}
+      constructorType={'stockChart'}
+      options={options}
+    />
+  </div>
 );
 
-render (<App />, document.getElementById('root'))
+render(<App />, document.getElementById('root'));
